61616899026ebcf5d2509bc5b2ad7bea6a6b0ab8
MaxAltena <Max@MaxAltena.com>
2019-03-02 21:49:10 +0100

    made feed dynamic

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,15 +1,60 @@
 import React from "react";
 import FeedPost from "./FeedPost";
 
+const posts = [
+  {
+    id: 1,
+    title: "Titel 1",
+    preview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusantium, quis culpa illo optio inventore. Hic mollitia ducimus possimus. Corrupti consequuntur animi esse eius tempore fugiat reiciendis minus dolores excepturi.",
+    image: "https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b",
+    author: "Max Altena",
+    date: "2 Maart 2019"
+  },
+  {
+    id: 2,
+    title: "Titel 2",
+    preview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusantium, quis culpa illo optio inventore. Hic mollitia ducimus possimus. Corrupti consequuntur animi esse eius tempore fugiat reiciendis minus dolores excepturi.",
+    image: "https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b",
+    author: "Max Altena",
+    date: "3 Maart 2019"
+  },
+  {
+    id: 3,
+    title: "Titel 3",
+    preview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusantium, quis culpa illo optio inventore. Hic mollitia ducimus possimus. Corrupti consequuntur animi esse eius tempore fugiat reiciendis minus dolores excepturi.",
+    image: "https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b",
+    author: "Max Altena",
+    date: "4 Maart 2019"
+  },
+  {
+    id: 4,
+    title: "Titel 4",
+    preview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusantium, quis culpa illo optio inventore. Hic mollitia ducimus possimus. Corrupti consequuntur animi esse eius tempore fugiat reiciendis minus dolores excepturi.",
+    image: "https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b",
+    author: "Max Altena",
+    date: "5 Maart 2019"
+  },
+  {
+    id: 5,
+    title: "Titel 5",
+    preview:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusantium, quis culpa illo optio inventore. Hic mollitia ducimus possimus. Corrupti consequuntur animi esse eius tempore fugiat reiciendis minus dolores excepturi.",
+    image: "https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b",
+    author: "Max Altena",
+    date: "6 Maart 2019"
+  }
+];
+
 const Feed = () => {
   return (
     <div className="Feed">
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {posts.map(post => {
+        return <FeedPost post={post} key={post.id} />;
+      })}
     </div>
   );
 };
--- src/components/Feed/FeedPost.js
+++ src/components/Feed/FeedPost.js
@@ -1,47 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const FeedPost = () => {
+const FeedPost = ({ post }) => {
+  const { id, title, preview, image, author, date } = post;
   return (
     <div className="FeedPost card">
       <div className="card-content">
-        <span className="card-title hind bold">FeedPost titel</span>
+        <span className="card-title hind bold">{title}</span>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
-          accusantium, quis culpa illo optio inventore. Hic mollitia ducimus
-          possimus. Corrupti consequuntur animi esse eius tempore fugiat
-          reiciendis minus dolores excepturi.{" "}
-          <Link to="/post/id">Verder lezen...</Link>
+          {preview} <Link to={`/post/${id}`}>Verder lezen...</Link>
         </p>
       </div>
       <div className="card-action">
         <div className="info hide-on-small-only">
-          <img
-            src="https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b"
-            alt="Auteur"
-            className="circle"
-          />
+          <img src={image} alt="Auteur" className="circle" />
           <div>
             <span>Auteur</span>
-            <span>Max Altena</span>
+            <span>{author}</span>
           </div>
           <div>
             <span>Gepubliceerd op</span>
-            <span>2 Maart 2019</span>
+            <span>{date}</span>
           </div>
         </div>
         <div className="info hide-on-med-and-up">
-          <img
-            src="https://www.gravatar.com/avatar/2bd431953d5fdbf5bc245dce12944a9b"
-            alt="Auteur"
-            className="circle"
-          />
+          <img src={image} alt="Auteur" className="circle" />
           <div>
-            <span>Max Altena</span>
-            <span>2 Maart 2019</span>
+            <span>{author}</span>
+            <span>{date}</span>
           </div>
         </div>
-        <Link to="/post/id" className="btn-flat waves-effect">
+        <Link to={`/post/${id}`} className="btn-flat waves-effect">
           Lees bericht
         </Link>
       </div>
